dee091e128a2c114fb916f0caaa6ce4a56ff0ccb
Sune Seifert <sse@abtion.com>
2025-12-17 23:19:29 +0100

    style: remove commented code in AuthButton component for cleaner code

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -5,15 +5,13 @@ import { LogoutButton } from "./logout-button";
 
 export async function AuthButton() {
   const supabase = await createClient();
-
-  // You can also use getUser() which will be slower.
   const { data } = await supabase.auth.getClaims();
 
   const user = data?.claims;
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+      <span className="max-md:hidden">Hey, {user.email}!</span>
       <LogoutButton />
     </div>
   ) : (
